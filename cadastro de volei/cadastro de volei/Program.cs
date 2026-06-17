64eddad6eb3c2e138c2c3f5189a14d2b0ae4c915
Julia Gutierres <juliagutierres1962@gmail.com>
2026-06-17 11:22:47 -0300

    Add files via upload

---
+++ cadastro de volei/cadastro de volei/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cadastro_de_volei
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(@"
+░█████╗░░█████╗░██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░  ██████╗░███████╗
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗  ██╔══██╗██╔════╝
+██║░░╚═╝███████║██║░░██║███████║╚█████╗░░░░██║░░░██████╔╝██║░░██║  ██║░░██║█████╗░░
+██║░░██╗██╔══██║██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║  ██║░░██║██╔══╝░░
+╚█████╔╝██║░░██║██████╔╝██║░░██║██████╔╝░░░██║░░░██║░░██║╚█████╔╝  ██████╔╝███████╗
+░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░  ╚═════╝░╚══════╝
+
+██╗░░░██╗░█████╗░██╗░░░░░███████╗██╗
+██║░░░██║██╔══██╗██║░░░░░██╔════╝██║
+╚██╗░██╔╝██║░░██║██║░░░░░█████╗░░██║
+░╚████╔╝░██║░░██║██║░░░░░██╔══╝░░██║
+░░╚██╔╝░░╚█████╔╝███████╗███████╗██║
+░░░╚═╝░░░░╚════╝░╚══════╝╚══════╝╚═╝");
+            Console.ResetColor();
+            Console.WriteLine("\n quantidade de paises: ");
+            string qntpaises = Console.ReadLine();
+            Console.WriteLine("\n quantidade de inscritos: ");
+            string qntinscritos = Console.ReadLine();
+            Console.WriteLine("\n qual o genero: ");
+            string genero = Console.ReadLine();
+            Console.WriteLine("\n qual a forma de posicionamento: ");
+            string posicionamento = Console.ReadLine();
+
+
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"
+██████╗░███████╗░██████╗░█████╗░██████╗░██╗░█████╗░░█████╗░░█████╗░
+██╔══██╗██╔════╝██╔════╝██╔══██╗██╔══██╗██║██╔══██╗██╔══██╗██╔══██╗
+██║░░██║█████╗░░╚█████╗░██║░░╚═╝██████╔╝██║██║░░╚═╝███████║██║░░██║
+██║░░██║██╔══╝░░░╚═══██╗██║░░██╗██╔══██╗██║██║░░██╗██╔══██║██║░░██║
+██████╔╝███████╗██████╔╝╚█████╔╝██║░░██║██║╚█████╔╝██║░░██║╚█████╔╝
+╚═════╝░╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░╚════╝░╚═╝░░╚═╝░╚════╝░");
+            Console.ResetColor();
+            Console.WriteLine("\n qual o local?: ");
+            string  local = Console.ReadLine();
+            Console.WriteLine("\n qual o horário?: ");
+            string horario = Console.ReadLine();
+            Console.WriteLine("\n qual a fase?: ");
+            string fase = Console.ReadLine();
+            Console.WriteLine("\n qual o confronto: ");
+            string confronto = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine(@"
+░█████╗░░█████╗░██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░██████╗░░█████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+██║░░╚═╝███████║██║░░██║███████║╚█████╗░░░░██║░░░██████╔╝███████║██║░░██║██║░░██║
+██║░░██╗██╔══██║██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██╔══██║██║░░██║██║░░██║
+╚█████╔╝██║░░██║██████╔╝██║░░██║██████╔╝░░░██║░░░██║░░██║██║░░██║██████╔╝╚█████╔╝
+░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚════╝░");
+            Thread.Sleep(2500); // pausa na programação por 2s
+        }
+    }
+}
---
+++ cadastro de volei/cadastro de volei/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("cadastro de volei")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cadastro de volei")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("d75a0553-feca-45a9-b703-c363b7b2af9f")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
